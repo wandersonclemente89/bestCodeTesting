10b4e62fb78a29d6a0d64ac90702ae33fd05e1f9
da Silva, Wanderson <wanderson.dasilva@pearson.com>
2020-10-31 17:32:37 -0300

    Adicionando a funcionalidade de gerar o token

--- GeoposteBE/src/test/java/br/com/geopostelight/steps/Helper.java
+++ GeoposteBE/src/test/java/br/com/geopostelight/steps/Helper.java
@@ -19,13 +19,26 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class Helper extends BaseTest {
 
+    private Functions functions = new Functions();
+
     @Step
     public void autenticacao(){
 
-        setRequestSpecification(given()
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIyIiwianRpIjoiYTM1ZGUzOWMzMTgxODUyNDQyOGE5NDgyYjI5YTBlNmZjZTY0N2U4MDE3NzI3MTY5NWFmOGE0NDZkZDNkYzFlMTQ1OTBiYTFhNjBjNWZhMTkiLCJpYXQiOjE2MDQwOTE4MDMsIm5iZiI6MTYwNDA5MTgwMywiZXhwIjoxNjM1NjI3ODAzLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.G4QqFrViejtbDhsbbIpxW2wm22BpM5jlQnuYU7JciBS7IQ2EcTUGZsvDhFaA3As-mnAmMKpS3P-wFSTnye0Db5PlnwKnZUoGdt9Wht97dtgKQu7TX8pNNB2AUTsL66VSqKRD3q9UeKKh4kCVeOTqDsDg8YN1x_yUrFo-pIciXBTjMPLVT0tLpYvAOzIpp60QGqf0zjQ2exnY_zJoAa13msYPwVFc8hm9HgmsSxIflE0Jw3acsEnUOP4Yli85RtWA0wmOHHZ1m0Q1Nb-JMFLMOmBf-zHEK9vochGgpFQlvmv80b2bbT_FIkfU2QR9vHWEgv3HLZh9q-4RH5Ry4P0D8_MPeHrbcjXhLNeaR5TQo2JQgzgUv7a0W6sGLX3jHuWD5QX--AgIA5M_q68vS4oFuClf7_DYRDZO_7MXiCLX9xmA0F0RMO2pPhfQBCQI_HhR-Tjq5BQV0rfj5_YgEoglyNt1PzZNsU25mQ8AG5x6n6DKUo1TQ9KckbbDIvh0g0i147kcxe1GnJ3XFEzxW1_gNEf-EP4MDbfBPzhzB7IpxJ5jCWXg6ZXYiU72VZc2T9nAiJo2dZMshW7WZLYgkPSRR-y7Ghogg4IcBt3alpx1evP2uOowzyzdWlIpFyfBVC4yFvL71-Ho8ZNw9U_xdg_zOUFWC5gyRWEpNG4ehNoXUWs")
-        );
+        try {
+            JSONObject payload = functions.getJson("src/test/resources/requests/gerarToken.json");
+            String token = given().contentType(ContentType.JSON).body(payload.toString())
+                    .when().post("/auth/login")
+                    .then().log().body()
+                    .extract().body().jsonPath().get("access_token");
+
+            setRequestSpecification(given()
+                    .contentType(ContentType.JSON)
+                    .header("Authorization", "Bearer "+ token)
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Step
@@ -96,7 +109,6 @@ public class Helper extends BaseTest {
     @Step
     public void configurarInformacoesDaEmpresa(DataTable dataTable) throws IOException {
         List<List<String>> linhas = dataTable.asLists(String.class);
-        Functions functions = new Functions();
 
         JSONObject payload = functions.getJson("src/test/resources/requests/adicionarEmpresa.json");
 
